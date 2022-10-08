082c04dd76ab4e8f22724def9684580608845c7b
woodaaron2001 <woodaaron2001@gmail.com>
2022-10-08 14:23:13 +0100

    Added initial verilog projects and z80 cpu files

---
+++ VERILOG PROJECTS/helloBasys/BCDConverter.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:40:56 10/06/2022 
+// Design Name: 
+// Module Name:    BCDConverter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module BCDConverter(
+    input clk,
+    input en,
+    input [11:0] bin_d_in,
+    output [15:0] bcd_d_out,
+    output rdy
+    );
+
+
+endmodule
---
+++ VERILOG PROJECTS/helloBasys/isim/stimulus_isim_beh.exe.sim/work/m_00000000004150087143_3823007873.c
@@ -0,0 +1,159 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/redbr/ise_projects/helloBasys/top.v";
+static int ng1[] = {1, 0};
+static int ng2[] = {4, 0};
+
+
+
+static void Always_28_0(char *t0)
+{
+    char t8[8];
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t9;
+
+LAB0:    t1 = (t0 + 2528U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(28, ng0);
+    t2 = (t0 + 3096);
+    *((int *)t2) = 1;
+    t3 = (t0 + 2560);
+    *((char **)t3) = t2;
+    *((char **)t1) = &&LAB4;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(29, ng0);
+
+LAB5:    xsi_set_current_line(30, ng0);
+    t4 = (t0 + 1608);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = ((char*)((ng1)));
+    memset(t8, 0, 8);
+    xsi_vlog_unsigned_add(t8, 32, t6, 26, t7, 32);
+    t9 = (t0 + 1608);
+    xsi_vlogvar_wait_assign_value(t9, t8, 0, 0, 26, 0LL);
+    goto LAB2;
+
+}
+
+static void Cont_33_1(char *t0)
+{
+    char t3[8];
+    char t7[8];
+    char *t1;
+    char *t2;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t8;
+    char *t9;
+    unsigned int t10;
+    unsigned int t11;
+    unsigned int t12;
+    unsigned int t13;
+    unsigned int t14;
+    unsigned int t15;
+    char *t16;
+    char *t17;
+    char *t18;
+    char *t19;
+    char *t20;
+    unsigned int t21;
+    unsigned int t22;
+    char *t23;
+    unsigned int t24;
+    unsigned int t25;
+    char *t26;
+    unsigned int t27;
+    unsigned int t28;
+    char *t29;
+
+LAB0:    t1 = (t0 + 2776U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(33, ng0);
+    t2 = ((char*)((ng2)));
+    t4 = (t0 + 1608);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    memset(t7, 0, 8);
+    t8 = (t7 + 4);
+    t9 = (t6 + 4);
+    t10 = *((unsigned int *)t6);
+    t11 = (t10 >> 25);
+    t12 = (t11 & 1);
+    *((unsigned int *)t7) = t12;
+    t13 = *((unsigned int *)t9);
+    t14 = (t13 >> 25);
+    t15 = (t14 & 1);
+    *((unsigned int *)t8) = t15;
+    xsi_vlog_mul_concat(t3, 4, 1, t2, 1U, t7, 1);
+    t16 = (t0 + 3192);
+    t17 = (t16 + 56U);
+    t18 = *((char **)t17);
+    t19 = (t18 + 56U);
+    t20 = *((char **)t19);
+    memset(t20, 0, 8);
+    t21 = 15U;
+    t22 = t21;
+    t23 = (t3 + 4);
+    t24 = *((unsigned int *)t3);
+    t21 = (t21 & t24);
+    t25 = *((unsigned int *)t23);
+    t22 = (t22 & t25);
+    t26 = (t20 + 4);
+    t27 = *((unsigned int *)t20);
+    *((unsigned int *)t20) = (t27 | t21);
+    t28 = *((unsigned int *)t26);
+    *((unsigned int *)t26) = (t28 | t22);
+    xsi_driver_vfirst_trans(t16, 0, 3);
+    t29 = (t0 + 3112);
+    *((int *)t29) = 1;
+
+LAB1:    return;
+}
+
+
+extern void work_m_00000000004150087143_3823007873_init()
+{
+	static char *pe[] = {(void *)Always_28_0,(void *)Cont_33_1};
+	xsi_register_didat("work_m_00000000004150087143_3823007873", "isim/stimulus_isim_beh.exe.sim/work/m_00000000004150087143_3823007873.didat");
+	xsi_register_executes(pe);
+}
